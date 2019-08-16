f7ce8588f1e807c914ece80622495d5aa3b968b5
doinghun <de.qtner@gmail.com>
2019-08-16 15:35:49 +0800

    feat: added task GET end point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+app.set('view engine', 'ejs');
 
-const app = express()
-app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('Hello')
+})
+
+app.get('/tasks', async (req,res) => {
+    const client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'postgres',
+      password: 'dohun1208',
+      port: 5432
+  })
+  await client.connect()
+  const { rows } = await client.query('SELECT * FROM "Article"')
+  await client.end()
+  res.send(rows)
 })
 
 const port = 3000
