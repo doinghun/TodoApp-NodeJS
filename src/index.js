ab364e3879cf13c90293c2cced183e60bb7fd504
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2019-08-17 13:14:07 +0800

    feat: add get tasks endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 const express = require('express')
-
 const app = express()
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-  res.send('hello')
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'todolist1234',
+  port: '5432'
 })
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Todo app (:')
+})
+
+app.get('/tasks', async (req, res) => {
+  await client.connect()
+  const data = await client.query('SELECT * from tasks')
+  await client.end()
+  res.send(data.rows)
+})
+
+/*
+app.post('/tasks', (req, res) => {
+  var task_title = req.body.title
+  console.log(task_title)
+  res.send(task_title)
+})
+*/
 
 const port = 3000
 app.listen(port, () => {
