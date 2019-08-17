574a114832cc093a22c49a161279e944be5653a7
Mootda <de.qtner@gmail.com>
2019-08-17 12:07:19 +0800

    feat: added GET /tasks  endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
-
 const app = express()
+const { Client } = require('pg');
+const config = require('../config');
+
 app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('hello')
+})
+
+app.get('/tasks', async (req, res) => {
+  const client = new Client({
+    user: config.user,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port:config.port
+  })
+  await client.connect()
+  const { rows } = await client.query('SELECT * FROM tasks')
+  await client.end()
+  res.send(rows)
 })
 
 const port = 3000
