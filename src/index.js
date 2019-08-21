1de995862813f8eec698ef197de6f0923b49c1b3
Mootda <de.qtner@gmail.com>
2019-08-22 00:50:35 +0800

    feat: edit views to add new task form & post endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const query = require('./lib/pg')
 const path = require('path')
-
-const app = express()
-app.set('view engine', 'ejs')
+const parser = require('body-parser');
+const app = express();
+app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
+
+app.use(parser.urlencoded({extended: false}));
+app.use(parser.json());
 
 app.get('/', (_req, res) => {
   res.render('index');
@@ -13,6 +16,13 @@ app.get('/', (_req, res) => {
 app.get('/tasks', async (_req, res) => {
   const { rows } = await query('SELECT id, title FROM tasks')
   res.render('index', { rows } );
+})
+
+app.post('/tasks/add', (req,res) => {
+  var task = {
+    task : req.body.task
+    }
+  res.json(task);
 })
 
 const port = 3000
