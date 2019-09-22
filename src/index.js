43557d4a4a2f233c276abd46f490904bd516e5da
Mootda <de.qtner@gmail.com>
2019-09-23 03:29:55 +0800

    authentication logic - hash password on sign up & match during login

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const query = pg.query
 const pgPool = pg.pgPool
 const path = require('path')
 const parser = require('body-parser')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 const uuidv4 = require('uuid/v4')
+const bcrypt = require('bcrypt')
+const flash = require('connect-flash')
 
 const app = express()
 
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(parser.urlencoded({ extended: false }))
 app.use(parser.json())
 
+app.use(flash())
 app.use(session({
   store: new pgSession({
     pool : pgPool,
@@ -27,18 +28,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false
   }))
-
-passport.use('local', new  LocalStrategy({passReqToCallback : true}, (req, username, password, done) => {
-  loginAttempt();
-  async function loginAttempt(){
-    query(`SELECT id, email, password FROM users WHERE email = ${email}, `)
-    .then(() => {
-      res.redirect('/tasks')
-    })
-    .catch(err => console.log(err))
-  }
-  
-}))
 
 app.use('/scripts', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/')))
 
@@ -50,7 +39,7 @@ app.get('/tasks', async (req, res) => {
   const show_all = req.query.show_all || "true"
   const { rows } = await query('SELECT id, title, is_done FROM tasks ORDER BY id ')
   const error = req.query.error || ""
-  res.render('index', { rows, error, show_all, isAuthenticated: req.session.isLoggedIn} )
+  res.render('index', { rows, error, show_all, isAuthenticated: req.session.isLoggedIn,  messages: {danger: req.flash('danger'), warning: req.flash('warning'), success: req.flash('success')}} )
 })
 
 app.post('/tasks/add', (req, res) => {
@@ -86,13 +75,38 @@ app.post('/tasks/update', (req,res) => {
 
 app.get('/login', (req, res) => {
   console.log(req.session.isLoggedIn)
-  res.render('login', { isAuthenticated: req.session.isLoggedIn })
+  res.render('login', { isAuthenticated: req.session.isLoggedIn,  messages: {danger: req.flash('danger'), warning: req.flash('warning'), success: req.flash('success')} })
 })
 
-app.post('/login', (req,res)=> {
+app.post('/login', async (req,res) => {
+  const email = req.body.email
+  const password = req.body.password
 
-  req.session.isLoggedIn = true;
-  res.redirect('/tasks')
+  query(`SELECT email, password FROM users WHERE email = '${email}'`)
+  .then(user => {
+    if(user.rows.length == 0){
+      req.flash('danger', "Oops. Incorrect login details.");
+      res.redirect('/login');
+    }
+    bcrypt
+    .compare(password,user.rows[0].password)
+    .then(doMatch => {
+      if(doMatch){
+        req.session.isLoggedIn = true
+        req.session.user = user
+        return req.session.save(err => {
+          console.log(err)
+          res.redirect('/tasks')
+          })
+        }
+      res.redirect('/login')
+      })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/login')
+      })
+    })
+  .catch(err => console.log(err))
 })
 
 app.post('/logout', (req,res)=>{
@@ -109,10 +123,12 @@ app.get('/signup', (req, res) => {
 app.post('/signup', (req, res) => {
   const email = req.body.email
   const password = req.body.password
-  query(`INSERT INTO users (id, email, password) VALUES ('${uuidv4()}','${email}','${password}')`)
+  const confirmPassword = req.body.confirmPassword
+  const hashedPassword = bcrypt.hashSync(password, 12)
+  query(`INSERT INTO users (id, email, password) VALUES ('${uuidv4()}','${email}','${hashedPassword}')`)
   .then(() => {
-    res.redirect('/login')
-  })
+      res.redirect('/login')
+    })
   .catch(err => console.log(err))
 })
 
