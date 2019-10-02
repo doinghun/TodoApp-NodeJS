38a45ab89a6aff92c41a72dee71a6d2895a818a7
Mootda <de.qtner@gmail.com>
2019-10-02 19:06:26 +0800

    change list to table layout

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,21 +1,28 @@
-const createLi = (task) => {
-    const li = document.createElement('li');
-    // add user details to `li`
-    li.textContent = `${task.title}`;
+const createTable = (task) => {
     const id = parseInt(task.id)
-    // attach onclick event
-    li.onclick = e => deleteTask(li, id); 
+    const table = document.getElementById('task-table');
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0)
+    var cell2 = row.insertCell(1)
+    var cell3 = row.insertCell(2)
 
-    return li;
-};
+    cell2.innerHTML = `${task.title}`;
+    var btn = document.createElement('input');
+    btn.type = "submit";
+    btn.className = "btn";
+    btn.value = "x"
+    cell3.appendChild(btn)
+    btn.onclick = e => deleteTask(cell3,id)
+
+    return table;
+}
 
 const appendToDOM = (tasks) => {
-    const ul = document.querySelector('ul');
-    //iterate over all users
+
     tasks.map(task => {
-        ul.appendChild(createLi(task));
-    });
-};
+        createTable(task);
+    })
+}
 
 const fetchTasks = () => {
     axios.get('/api/tasks')
@@ -44,25 +51,27 @@ const createTask = (task) => {
 };
 
 // event listener for form submission
-const form = document.querySelector('form');
+const form = document.getElementById('submit-task');
 
 const formEvent = form.addEventListener('submit', event => {
     event.preventDefault();
-
+    
     const title = document.querySelector('#task').value;
 
     const row = { title };
     createTask(row);
+    form.reset();
 });
 
 
 // delete a task
 const deleteTask = (elem, id) => {
+    var i = elem.parentNode.rowIndex;
     axios.delete(`/api/tasks/${id}`)
         .then(res => {
             console.log(`DELETE: task is removed`, id);
             // remove elem from DOM
-            elem.remove();
+            document.getElementById('task-table').deleteRow(i)
         })
         .catch(error => console.error(error));
 };
